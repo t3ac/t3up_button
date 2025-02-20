ad16f899a5616c20824619994346336514de5a42
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-02-20 12:35:42 +0100

    Add files via upload

---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "t3up_button".
+ *
+ * 15-10-2024 
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+$EM_CONF[$_EXTKEY]  = [
+  	'title' 			=> 'T3UP - Buttons',
+  	'description' 		=> 'T3UP for EXT:container',
+	'category' 			=> 'templates',
+	'author' 			=> 'Michael Lang',
+  	'author_email' 		=> 'michael.lang@h-da.de',
+    'version'           => '12.1.01',
+  	'author_company' 	=> 'h_da',
+  	'state' 			=> 'stable',
+    'uploadfolder'    	=> false,
+    'createDirs'       	=> '',
+    'clearCacheOnLoad' 	=> true,
+    'constraints'      	=> [
+        'depends'   => [
+            'typo3'          => '12.4.0-',
+            'container'		 =>	'3-',
+            't3up'           => '12.0-',
+        ],
+        'conflicts' => [],
+        'suggests'  => [],
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('TYPO3') || die('Access denied.');
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+call_user_func(static function () {
+    
+    // Add PageTSConfig
+	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_button/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig'");
+    
+    // Register icons
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    
+    $iconRegistry->registerIcon(
+        'headerbutton_icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:t3up_button/Resources/Public/Icons/headerbutton_icon.svg']
+   );
+    
+    $iconRegistry->registerIcon(
+        'textbutton_icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:t3up_button/Resources/Public/Icons/textbutton_icon.svg']
+        );   
+    
+    $iconRegistry->registerIcon(
+        'imagebutton_icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:t3up_button/Resources/Public/Icons/imagebutton_icon.svg']
+        ); 
+    
+    $iconRegistry->registerIcon(
+        'texttile_icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:t3up_button/Resources/Public/Icons/texttile_icon.svg']
+        ); 
+});
