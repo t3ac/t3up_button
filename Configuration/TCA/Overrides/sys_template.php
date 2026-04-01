911e45ac77285087fcbcd5a4ef4b3857778fe447
Michael Lang <113889180+t3ac@users.noreply.github.com>
2026-04-01 12:05:50 +0200

    Delete Configuration directory

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    't3up_button', 'Configuration/TypoScript/', 'T3UP - Buttons (Bootstrap)'
-);
